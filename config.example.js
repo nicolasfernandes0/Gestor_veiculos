e157bb7a12ca8e5fc6817980a72881a0f7b3e502
nicolasfernandes0 <99373328+nicolasfernandes0@users.noreply.github.com>
2025-09-23 17:37:28 +0000

    config

--- config.example.js
+++
@@ -1,5 +0,0 @@
-// Copie este arquivo para supabase-config.js e configure com suas credenciais
-export const supabaseConfig = {
-    url: 'https://kelzcwwdntxuplvqgjdg.supabase.co',
-    anonKey: 'sua-chave-anon-aqui'
-};
