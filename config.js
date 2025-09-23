a786690989246c0ab3575eefe535c7d9fd94ce72
nicolasfernandes0 <99373328+nicolasfernandes0@users.noreply.github.com>
2025-09-23 16:37:26 +0000

    config

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js - ARQUIVO SENSível - NÃO VERSIONAR NO GIT
+export const supabaseConfig = {
+    url: 'https://kelzcwwdntxuplvqgjdg.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlbHpjd3dkbnR4dXBsdnFnamRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyMDQ5ODcsImV4cCI6MjA3MDc4MDk4N30.KO6zEZXewXk2UUkjPEB3vDKoFXryIrEPmaefv58nCkg'
+};
