ea9be322f4fa2081398ee4f51d844f0a48cd6329
nicolasfernandes0 <99373328+nicolasfernandes0@users.noreply.github.com>
2025-10-07 01:12:38 +0000

    delete

--- supabase-config.js
+++
@@ -1,5 +0,0 @@
-// Copie este arquivo para supabase-config.js e configure com suas credenciais
-export const supabaseConfig = {
-    url: 'https://kelzcwwdntxuplvqgjdg.supabase.co',
-    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtlbHpjd3dkbnR4dXBsdnFnamRnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTUyMDQ5ODcsImV4cCI6MjA3MDc4MDk4N30.KO6zEZXewXk2UUkjPEB3vDKoFXryIrEPmaefv58nCkg'
-};
